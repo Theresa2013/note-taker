b88f5cf2729e8910f99b943dbb926baa753a0727
Theresa2013 <tab61295@gmail.com>
2022-07-31 14:20:17 -0500

    getting notes to save

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
+// dependencies
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require("fs");
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// routes to route files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}!`);
